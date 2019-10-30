2dc40180c5acdbe0e982aff008bc41e548bfd98e
Marek Kaliszczyk <marek.kaliszczyk@gmail.com>
2019-10-30 21:02:32 +0100

    fixed install script

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,3 +1,16 @@
+#install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+echo "oh-my-zsh installed"
+
+#install custom aphrodite theme
+mkdir -p ~/.oh-my-zsh/custom/themes/
+wget -xqO ~/.oh-my-zsh/custom/themes/aphrodite.zsh-theme https://raw.githubusercontent.com/codesecurely/dotfiles/master/config/zsh/custom/themes/aphrodite.zsh-theme
+sed -i.bak 's/^[[:space:]]*ZSH_THEME=.*/ZSH_THEME="aphrodite"/' ~/.zshrc
+
+echo "custom theme installed"
+
+#install the dotfiles
 for dotfile in $(pwd)/files/* 
 do
     file=$(basename $dotfile)
@@ -11,4 +24,3 @@ done
 
 mkdir -p ~/.vim/colors
 ln -svf $(pwd)/config/vim/colors/molokai.vim ~/.vim/colors/molokai.vim
-source ~/.bashrc
