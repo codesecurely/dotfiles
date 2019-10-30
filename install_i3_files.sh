b9ebb81505a54e870d1ebeb59fe2046b571fd337
Marek Kaliszczyk <marek.kaliszczyk@gmail.com>
2019-10-30 21:00:45 +0100

    added zsh and alacritty

---
+++ install_i3_files.sh
@@ -0,0 +1,7 @@
+ln -s ~/dotfiles/files/i3.conf ~/regolith/i3/config
+ln -s ~/dotfiles/files/i3xrocks_bar1.conf ~/regolith/i3xrocks/config
+ln -s ~/dotfiles/files/i3xrocks_bar2.conf ~/regolith/i3xrocks/config_second
+ln -s ~/dotfiles/files/compton.conf ~/regolith/compton/config
+ln -s ~/dotfiles/files/conky.conf ~/regolith/conky/config
+
+
